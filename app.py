68f999897be1d0091e76d480bddd23e33665c6e7
Xi Xi Lin <>
2021-06-03 19:20:38 +0000

    --

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-
-# Blueprints
-from base.base import base_bp
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
-app.register_blueprint(base_bp)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/documentation')
+def documentation():
+  return render_template('documentation.html')
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+  app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for replit to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
