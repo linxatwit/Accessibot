ca2c8e29509bd0849d5c0ad5259a9d04aef78484
Xi Xi Lin <42873025+linxatwit@users.noreply.github.com>
2021-06-03 16:49:50 -0400

    page navigation

--- app.py
+++ app.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/documentation')
 def documentation():
   return render_template('documentation.html')
 
+@app.route('/commands')
+def commands():
+  return render_template('commands.html')
+
+@app.route('/feedback')
+def feedback():
+  return render_template('feedback.html')
+
 if __name__ == '__main__':
-  app.run( # Starts the site
-		host='0.0.0.0',  # EStablishes the host, required for replit to detect the site
-		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+  app.config['ENV'] = 'dev'  
+  app.config['DEBUG'] = True
+  app.run(threaded=True# Starts the site
+		#host='0.0.0.0',  # EStablishes the host, required for replit to detect the site
+		#port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
 	)
