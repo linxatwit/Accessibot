8faddd27a0f5af10f74161cd70d46a16b2264831
Xi Xi Lin <42873025+linxatwit@users.noreply.github.com>
2021-06-20 16:58:45 -0400

    added flask mail, updated index with image

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from flask_mail import Mail, Message
 import random
 
 app = Flask(__name__)
+
+# gmail configurations
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'accessibotdiscord@gmail.com'
+app.config['MAIL_PASSWORD'] = 'seniorproject2021'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -15,8 +26,45 @@ def documentation():
 def commands():
   return render_template('commands.html')
 
-@app.route('/feedback')
+@app.route('/feedback', methods=["GET", "POST"])
 def feedback():
+
+  if request.method == 'POST':
+    # User inputs from form
+    inputs = {}
+    inputs['discordTag'] = request.form['discordTag']
+    inputs['categories'] = request.form['categories']
+    inputs['description'] = request.form['description']
+
+    subject = ""
+
+    if inputs['categories'] == "bot":
+      subject = "Bot Functionalities"
+    if inputs['categories'] == "feedback":
+      subject = "Feedback/Suggestions"
+    if inputs['categories'] == "technical":
+      subject = "Technical Issues"
+    if inputs['categories'] == "ui":
+      subject = "User Interface"
+    if inputs['categories'] == "web":
+      subject = "Website Support"
+    if inputs['categories'] == "other":
+      subject = "Other"
+
+    msg = Message(subject,
+                    sender="accessibotwebsite@accesibot.com",
+                    recipients=["accessibotdiscord@gmail.com"])
+
+    msg.html="""
+    <h2 style="font-weight: normal">You have received a new feedback.</h2>
+
+    <p style="font-size: 15px"><b>Discord Tag:</b> {}</p>
+    <p style="font-size: 15px"><b>Message:</b> {}</p>
+    """.format(inputs['discordTag'], inputs['description'])
+
+    mail.send(msg)
+    return render_template('feedback.html')
+
   return render_template('feedback.html')
 
 if __name__ == '__main__':
