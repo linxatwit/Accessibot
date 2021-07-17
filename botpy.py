6bdc5fa4572d41b1c27786114f89262e3a6fe657
Wooten <wootenl@wit.edu>
2021-07-17 09:05:10 -0400

    Oh wow I might have done it

--- botpy.py
+++ botpy.py
@@ -95,8 +95,7 @@ async def on_message(message):
         # https://www.ffmpeg.org/download.html -> Windows Build by BtbN -> ffmpeg-n4.4-72-g91aa49218e-win64-gpl-4.4.zip -> bin -> copy exe files to directory
         # use exe file shown below with file path + mp3 source
         if not voiceConnection.is_playing():
-          # pitchOptions = "-af asetrate=" + 48000*(serverBotInfo.get("pitch")) + ",atempo=1,aresample=44100"
-          pitchOptions = '-af atempo=1'
+          pitchOptions = "-af asetrate=" + int(48000*float(serverBotInfo.get("pitch"))) + ",atempo=1,aresample=44100"
           voiceConnection.play(discord.FFmpegPCMAudio(source = './input.mp3', options = pitchOptions))
           # wait until finish playing to delete
           while voiceConnection.is_playing():
