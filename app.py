7dbe66ac0ae5add06d037fc2f1ac32d21b0ef515
Xi Xi Lin <42873025+linxatwit@users.noreply.github.com>
2021-07-24 14:06:19 -0400

    updating environmental variable

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request, flash
 from flask_mail import Mail, Message
 import random
+import os
 
 app = Flask(__name__)
 
 # gmail configurations
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'xxx'
-app.config['MAIL_PASSWORD'] = 'xxx'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
