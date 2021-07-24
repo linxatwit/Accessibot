4f2363e214e379ee12f9c328a5492ba82b6778c4
Xi Xi Lin <42873025+linxatwit@users.noreply.github.com>
2021-07-24 15:10:58 -0400

    aaa

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app.config.update(dict(
     MAIL_PORT = 465,
     MAIL_USE_TLS = False,
     MAIL_USE_SSL = True,
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME'),
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD'),
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
 ))
 
 mail = Mail(app)
